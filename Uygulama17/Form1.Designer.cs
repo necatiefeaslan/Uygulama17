0f2c43e1a602658e0cdc27943cde3c80ca133829
necati148 <necati148@RPC11>
2024-03-05 10:10:31 +0300

    Proje dosyası ekle.

---
+++ Uygulama17/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Uygulama17
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnumara = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtDersNotu = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ders Notu";
+            // 
+            // txtnumara
+            // 
+            this.txtnumara.Location = new System.Drawing.Point(129, 55);
+            this.txtnumara.Name = "txtnumara";
+            this.txtnumara.Size = new System.Drawing.Size(100, 20);
+            this.txtnumara.TabIndex = 3;
+            this.txtnumara.Validating += new System.ComponentModel.CancelEventHandler(this.txtnumara_Validating);
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(129, 93);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(163, 20);
+            this.txtAdSoyad.TabIndex = 4;
+            this.txtAdSoyad.Validating += new System.ComponentModel.CancelEventHandler(this.txtAdSoyad_Validating);
+            // 
+            // txtDersNotu
+            // 
+            this.txtDersNotu.Location = new System.Drawing.Point(129, 129);
+            this.txtDersNotu.Name = "txtDersNotu";
+            this.txtDersNotu.Size = new System.Drawing.Size(100, 20);
+            this.txtDersNotu.TabIndex = 5;
+            this.txtDersNotu.Validating += new System.ComponentModel.CancelEventHandler(this.txtDersNotu_Validating);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(129, 170);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(100, 33);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "KAYDET";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 227);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtDersNotu);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.txtnumara);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Veri Doğrulama 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnumara;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtDersNotu;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
+
---
+++ Uygulama17/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama17
+{
+    public partial class Form1 : Form
+    {
+        ErrorProvider errorProvider = new ErrorProvider();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtnumara_Validating(object sender, CancelEventArgs e)
+        {
+            if (int.TryParse(txtnumara.Text, out int sonuc))
+            {
+                errorProvider.SetError(txtnumara, "");
+            }
+            else
+            {
+                e.Cancel = true;
+                errorProvider.SetError(txtnumara, "Numara girişi hatalı");
+            }
+        }
+
+        private void txtAdSoyad_Validating(object sender, CancelEventArgs e)
+        {
+            {
+                if (txtAdSoyad.Text == "")
+                {
+                    e.Cancel = true;
+                    errorProvider.SetError(txtAdSoyad, "Adı ve soyadı giriniz.");
+                }
+                else
+                {
+                    errorProvider.SetError(txtAdSoyad, "");
+                }
+            }
+        }
+
+        private void txtDersNotu_Validating(object sender, CancelEventArgs e)
+        {
+            int dersNotu;
+            if (int.TryParse(txtDersNotu.Text, out dersNotu))
+            {
+                if (dersNotu < 0 || dersNotu > 100)
+                {
+                    e.Cancel = true;
+                    errorProvider.SetError(txtDersNotu, "0 - 100 arasında değer giriniz.");
+                }
+                else
+                {
+                    errorProvider.SetError(txtDersNotu, "");
+                }
+            }
+                else
+            {
+                e.Cancel = true;
+                errorProvider.SetError(txtDersNotu, "Sayısal değer giriniz.");
+            }
+        }
+    }
+}
+    
+
